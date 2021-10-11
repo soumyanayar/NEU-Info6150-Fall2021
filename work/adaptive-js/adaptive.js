b906cd6be36f74da67ec2e2aa5b6f453df6bfa5c
Soumya Nayar <nayar.s@northeastern.edu>
2021-10-11 16:58:08 -0700

    Additional logic

--- work/adaptive-js/adaptive.js
+++ work/adaptive-js/adaptive.js
@@ -1,24 +1,38 @@
 let count = 0;
 let previous;
 
-const infoReport = document.querySelector(".info__report");
-const button = document.querySelector(".add-form__submit");
-
 // Removing the default behavior of form submit inorder to avoid
 // form elements being reset
 let form = document.querySelector(".add-form");
 form.addEventListener("submit", (event) => event.preventDefault());
 
+const infoReport = document.querySelector(".info__report");
+let button = document.querySelector(".add-form__submit");
+
+button.disabled = true;
+let textBox = document.querySelector(".label__input");
+let infoText = document.querySelector(".info__report");
+let infoTextDefaultValue = infoText.textContent;
+
+textBox.addEventListener("keyup", () => {
+  if (textBox.value !== "") {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+    infoText.innerHTML = infoTextDefaultValue;
+    count = 0;
+  }
+});
+
 button.addEventListener("click", () => {
-  let labelValue = document.querySelector(".label__input").value;
-  if (labelValue) {
-    if (labelValue !== previous) {
+  let textBoxValue = textBox.value.trim();
+  if (textBoxValue) {
+    if (textBoxValue !== previous) {
       count = 0;
     }
 
     ++count;
-    let textChange = document.querySelector(".info__report");
-    textChange.innerHTML = `The ${labelValue} has ${count} count`;
-    previous = labelValue;
+    infoText.innerHTML = `The ${textBoxValue} has ${count} count`;
+    previous = textBoxValue;
   }
 });
