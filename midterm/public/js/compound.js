a66417d0d9176c45d204247f40c755fe0d279e3b
Soumya Nayar <nayar.s@northeastern.edu>
2021-10-28 22:17:19 -0700

    Validation of Compund Interest

--- midterm/public/js/compound.js
+++ midterm/public/js/compound.js
@@ -1,69 +1,34 @@
-const contributionType = document.getElementsByName("term-type");
-const deposit = document.querySelector("#deposit-input");
-const contribution = document.querySelector("#contribution-input");
+const principal = document.querySelector("#deposit-input");
+const compoundsPerYear = document.querySelector("#contribution-input");
 const interestRate = document.querySelector("#apr");
 const numberOfTerms = document.querySelector("#terms-input");
 const calculateBtn = document.querySelector(".calculate-btn");
-const mortgageOutput = document.querySelector(".output");
-
-calculateBtn.disabled = true;
-
-const validateAllInputsArePresent = () => {
-  if (
-    deposit.value !== "" &&
-    contribution.value !== "" &&
-    interestRate.value !== ""
-  ) {
-    calculateBtn.disabled = false;
-  } else {
-    calculateBtn.disabled = true;
-  }
-  if (deposit.value < 0) {
-    deposit.value = "";
-  }
-  if (contribution.value < 0) {
-    contribution.value = "";
-  }
-  if (interestRate.value >= 100) {
-    interestRate.value = "";
-  }
-  if (interestRate.value < 0) {
-    interestRate.value = "";
-  }
-  if (numberOfTerms.value < 1) {
-    numberOfTerms.value = "";
-  }
-  if (parseFloat(deposit.value) < parseFloat(contribution.value)) {
-    contribution.value = "";
-  }
+const compoundOutput = document.querySelector(".output");
+const compundForm = document.getElementById("compound-form");
+compundForm.onsubmit = (event) => {
+  event.preventDefault();
+  calculateCompoundInterest();
+  return false;
 };
 
-deposit.onkeyup = validateAllInputsArePresent;
-contribution.onkeyup = validateAllInputsArePresent;
-interestRate.onkeyup = validateAllInputsArePresent;
-numberOfTerms.onkeyup = validateAllInputsArePresent;
-
-const calculateMortgage = () => {
-  const depositCurrent = parseFloat(deposit.value);
-  const contributionCurrent = parseFloat(contribution.value) * 12;
+const calculateCompoundInterest = () => {
+  const depositCurrent = parseFloat(principal.value);
+  const compoundsPerYearCurrent = parseFloat(compoundsPerYear.value) * 12;
   const currentInterestRate = parseFloat(interestRate.value) / 100;
   const currentnumberOfTerms = parseInt(numberOfTerms.value);
   if (
     isNaN(depositCurrent) ||
-    isNaN(contributionCurrent) ||
-    isNaN(currentInterestRate)
+    isNaN(compoundsPerYearCurrent) ||
+    isNaN(currentInterestRate) ||
+    isNaN(currentnumberOfTerms)
   ) {
-    mortgageOutput.innerHTML = 0;
+    compoundOutput.innerHTML = "";
   } else {
-    const paymentTermsPerYear = currentnumberOfTerms * 12;
+    const compoundRatio = currentInterestRate / compoundsPerYearCurrent;
 
-    const mortgageRatio = currentInterestRate / contributionCurrent;
-
-    let mortgage =
+    let compundAmount =
       depositCurrent *
-      (1 + mortgageRatio) ** (contributionCurrent * currentnumberOfTerms);
-    mortgageOutput.value = mortgage;
+      (1 + compoundRatio) ** (compoundsPerYearCurrent * currentnumberOfTerms);
+    compoundOutput.value = compundAmount.toFixed(2);
   }
 };
-
-calculateBtn.addEventListener("click", calculateMortgage);
