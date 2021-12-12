f2f224d0949a915084f60d2caac49a5ed4d7b820
Soumya Nayar <nayar.s@northeastern.edu>
2021-12-12 15:34:50 -0800

    few chsnges

---
+++ final/final-project/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Loading() {
+  return <main>Loading.....</main>;
+}
+
+export default Loading;
--- final/final-project/src/pages/Recipes.jsx
+++ final/final-project/src/pages/Recipes.jsx
@@ -1,52 +1,61 @@
-import React from 'react'
-import {useState, useEffect} from "react";
-import Recipe from '../components/Recipe';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import Recipe from "../components/Recipe";
+import Loading from "../components/Loading";
 import env from "react-dotenv";
 
 const Recipes = () => {
   const [recipe, setRecipe] = useState([]);
   const [query, setQuery] = useState("oats");
   let recipeStore;
-  useEffect(() => {
-    getRecipes();
-  },[setQuery]);
 
   const getRecipes = async () => {
-      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${env.APP_ID}&app_key=${env.APP_KEY}`);
-      recipeStore = await response.json();
-      setRecipe(recipeStore.hits);
-  }
+    const response = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${env.APP_ID}&app_key=${env.APP_KEY}`
+    );
+    recipeStore = await response.json();
+    setRecipe(recipeStore.hits);
+  };
 
   const searchRecipes = (e) => {
     e.preventDefault();
     getRecipes();
-  }
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, [setQuery]);
 
   return (
     <section className="recipe-search">
       <h2 className="section-title">Find Your Favourite Food Recipe</h2>
       <form className="search-form" onSubmit={searchRecipes}>
         <div className="form-control">
-        <input className="search-input"type="text" onChange={(e) => {
-          setQuery(e.target.value);
-          // console.log(e.target.value);
-        }}/>
-        <button type="submit" className="btn">Search</button>
+          <input
+            className="search-input"
+            type="text"
+            onChange={(e) => {
+              setQuery(e.target.value);
+              // console.log(e.target.value);
+            }}
+          />
+          <button type="submit" className="btn">
+            Search
+          </button>
         </div>
       </form>
       <div className="recipe-center">
-      {recipe.map((item, key) => (
-        <Recipe key={item.recipe.label}
-         image={item.recipe.image} 
-          title={item.recipe.label}    
-          ingredients = {item.recipe.ingredients}>
-          </Recipe> 
-         
-  ))}   
-   </div> 
+        {recipe.map((item, key) => (
+          <Recipe
+            key={item.recipe.label}
+            image={item.recipe.image}
+            title={item.recipe.label}
+            ingredients={item.recipe.ingredients}
+          ></Recipe>
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Recipes;
