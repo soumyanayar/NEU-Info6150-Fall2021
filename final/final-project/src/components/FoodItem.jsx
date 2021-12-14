b28fc0b10e2869bd240f07c7ec301dc1596a887c
Soumya Nayar <nayar.s@northeastern.edu>
2021-12-13 16:52:10 -0800

    Chnge styling of food item

--- final/final-project/src/components/FoodItem.jsx
+++ final/final-project/src/components/FoodItem.jsx
@@ -22,32 +22,63 @@ const FoodItem = () => {
     <div>
       {nutrients && (
         <div className="food-container">
-          <h3>{nutrients.recipe.label}</h3>
-          <img src={nutrients.recipe.image} alt="name"></img>
-          <div>
-            {Object.keys(nutrients.recipe.totalNutrients).map((keyName, i) => (
-              <li key={i}>
-                <span>
-                  {nutrients.recipe.totalNutrients[keyName].label}
-                  {"    "}
-                  {nutrients.recipe.totalNutrients[keyName].quantity.toFixed(2)}
-                  {nutrients.recipe.totalNutrients[keyName].unit}
-                </span>
-              </li>
-            ))}
-          </div>
-          <div>
-            <h4>Daily Value</h4>
-            {Object.keys(nutrients.recipe.totalDaily).map((keyName, i) => (
-              <li key={i}>
-                <span>
-                  {nutrients.recipe.totalDaily[keyName].label}
-                  {"    "}
-                  {nutrients.recipe.totalDaily[keyName].quantity.toFixed(2)}
-                  {nutrients.recipe.totalDaily[keyName].unit}
-                </span>
-              </li>
-            ))}
+          <h3 className="food-title">{nutrients.recipe.label}</h3>
+          <div className="food-img-details-container">
+            <div>
+              <img
+                className="food-img"
+                src={nutrients.recipe.image}
+                alt="name"
+              ></img>
+              <h6 className="sub-title">
+                Cuisine type : {nutrients.recipe.cuisineType}
+              </h6>
+              <h6 className="sub-title">
+                Meal type : {nutrients.recipe.mealType}
+              </h6>
+              <h6 className="sub-title">
+                Serving quantity : {nutrients.recipe.totalWeight.toFixed(2)}{" "}
+                grams
+              </h6>
+            </div>
+            <div className="food-sub-container">
+              <h4 className="sub-title">Nutrition Facts</h4>
+              {Object.keys(nutrients.recipe.totalNutrients).map(
+                (keyName, i) => (
+                  <li key={i}>
+                    <span className="food-nutrition-span-main">
+                      <span className="food-nutrition-span-label">
+                        {nutrients.recipe.totalNutrients[keyName].label}
+                        {"   :  "}
+                      </span>
+                      <span className="food-nutrition-span-qty">
+                        {nutrients.recipe.totalNutrients[
+                          keyName
+                        ].quantity.toFixed(2)}
+                        {nutrients.recipe.totalNutrients[keyName].unit}
+                      </span>
+                    </span>
+                  </li>
+                )
+              )}
+            </div>
+            <div className="food-sub-container">
+              <h4 className="sub-title">Daily Value</h4>
+              {Object.keys(nutrients.recipe.totalDaily).map((keyName, i) => (
+                <li key={i}>
+                  <span className="food-nutrition-span-main">
+                    <span className="food-nutrition-span-label">
+                      {nutrients.recipe.totalDaily[keyName].label}
+                      {"  :  "}
+                    </span>
+                    <span className="food-nutrition-span-qty">
+                      {nutrients.recipe.totalDaily[keyName].quantity.toFixed(2)}
+                      {nutrients.recipe.totalDaily[keyName].unit}
+                    </span>
+                  </span>
+                </li>
+              ))}
+            </div>
           </div>
         </div>
       )}
